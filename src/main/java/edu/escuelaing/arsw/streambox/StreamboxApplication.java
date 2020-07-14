104556384c43483f68eeac1271410eb7fc751faa
candresra <carlos.ramirez-t@mail.escuelaing.edu.co>
2020-07-14 00:21:03 -0500

    :rocket: - config port heroku

--- src/main/java/edu/escuelaing/arsw/streambox/StreamboxApplication.java
+++ src/main/java/edu/escuelaing/arsw/streambox/StreamboxApplication.java
@@ -3,10 +3,21 @@ package edu.escuelaing.arsw.streambox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class StreamboxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StreamboxApplication.class, args);
+        SpringApplication app = new SpringApplication(StreamboxApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
